1949d51dfb4b0bd5051eed30916e3c3313332d4b
IAnjy <ianjymartial@gmail.com>
2021-12-27 08:19:18 +0300

    mise en place base de données et formation du ressource client

---
+++ src/Entity/Client.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * @ApiResource
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numCompte;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenoms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $solde;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumCompte(): ?string
+    {
+        return $this->numCompte;
+    }
+
+    public function setNumCompte(string $numCompte): self
+    {
+        $this->numCompte = $numCompte;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenoms(): ?string
+    {
+        return $this->prenoms;
+    }
+
+    public function setPrenoms(string $prenoms): self
+    {
+        $this->prenoms = $prenoms;
+
+        return $this;
+    }
+
+    public function getSolde(): ?int
+    {
+        return $this->solde;
+    }
+
+    public function setSolde(int $solde): self
+    {
+        $this->solde = $solde;
+
+        return $this;
+    }
+}
